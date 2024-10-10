cc84f55c35565f2e0cc45330fb46bd830a4e5a2d
sj_dev <shalini.jain@blattnertech.com>
2024-10-10 12:22:58 +0530

    - first commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# flutter_dart_define_demo
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+
+
+###
+
+The main advantage of using --dart-define is that we're no longer hardcoding sensitive keys in the source code.
+dart-define provides a way to add the secrets to artifacts at build time so they do not need to be part of the source code.
+
+--release
+--flavor=staging
+--dart-define=app.flavor=staging
+--dart-define-from-file="values.json"
+--dart-define-from-file=".env"
+--dart-define=SOME_VAR=SOME_VALUE
+--dart-define=DEBUG=true
+
+
+To fetch Url value from file
+
+Note: --dart-define-from-file supports both .env and json files
+
+For example:
+
+- Create json file 
+- add --dart-define-from-file="values.json"  into additional run args in edit configuration section
+
+- Create .env file
+- add --dart-define-from-file=".env"  into additional run args in edit configuration section
+
+To add flavor using dart define
+
+For example:
+
+- add --flavor=staging --dart-define=app.flavor=staging  into additional run args in edit configuration section
+
+To store some value in dart define
+--dart-define=SOME_VAR=SOME_VALUE
+
+To fetch values using .env and obfuscation
+reference article
+https://codewithandrea.com/articles/flutter-api-keys-dart-define-env-files/
+https://pub.dev/packages/envied
+
+dart run build_runner clean
+dart run build_runner build --delete-conflicting-outputs
+
+apk reverse engineering 
+
+For example:
+
+flutter build apk --dart-define=SECRET_KEY=SEKRET --flavor=staging
+cd build/app/outputs/flutter-apk/
+mkdir app
+cd app
+cd lib
+cd x86_64
+
+
+strings libapp.so | grep init
+or
+grep SEKRET libapp.so
+
+or for example
+grep services libapp.so
+output: Binary file libapp.so matches
+
+or
+strings libapp.so | grep init
+
+"services" is the term we are searching for 
+
+reference : https://stackoverflow.com/questions/69346395/are-compile-time-variables-secure-in-flutter/69349448#69349448
+
+
+
+
+Alternate
+- place keys on server , fetch on runtime
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+import 'env.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // title: 'Flutter Dart Define Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Dart Define Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// m1
+  static const backendUrl =
+      String.fromEnvironment('backend_url', defaultValue: 'SOME_DEFAULT_VALUE');
+
+  ///m2
+  // static const backendUrl =
+  // String.fromEnvironment('backend_url');
+  //
+  // if (backend_url.isEmpty) {
+  // throw AssertionError('backend_url is not set');
+  // }
+
+  static const String flavor = String.fromEnvironment('app.flavor');
+
+  static const SOME_VAR =
+      String.fromEnvironment('SOME_VAR', defaultValue: 'SOME_DEFAULT_VALUE');
+  static const token =
+      String.fromEnvironment('TOKEN', defaultValue: 'SOME_DEFAULT_VALUE');
+
+  void log(String message) {
+    // Log the debug message if the environment declaration 'DEBUG' is `true`.
+    // If there was no value specified, do not log.
+    if (const bool.fromEnvironment('DEBUG', defaultValue: false)) {
+      print('debug: $message');
+    }
+  }
+
+  @override
+  void initState() {
+    log('debug mode value is true');
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Env obj = Env();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '1) URL value from json file',
+              ),
+              Text(
+                backendUrl,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '2) fetch flavor value',
+              ),
+              Text(
+                flavor,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '3) store some value',
+              ),
+              Text(
+                SOME_VAR,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '4) token value from .env file',
+              ),
+              Text(
+                token,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                '5) token value using .env file and obfuscation',
+              ),
+              Text(
+                obj.token,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
